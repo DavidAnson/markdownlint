7142bbd8ed85e5c6e42bd62c54b6a9a2c97d54d0
David Anson <david@dlaa.me>
2023-01-12 21:06:58 -0800

    Normalize line endings to get micromark test passing on Windows.

--- test/markdownlint-test-micromark.mjs
+++ test/markdownlint-test-micromark.mjs
@@ -1,10 +1,11 @@
 import fs from "node:fs/promises";
 import test from "ava";
+import newLineRe from "../helpers/helpers.js";
 import { parse } from "../lib/micromark.mjs";
 
 test("parse", async(t) => {
   t.plan(1);
   const content = await fs.readFile("./test/every-markdown-syntax.md", "utf8");
-  const document = parse(content);
+  const document = parse(content.split(newLineRe).join("\n"));
   t.snapshot(document, "Unexpected tokens");
 });
