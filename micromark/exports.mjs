e9aea98ea49445e6ce5fd9b163102ab8f9621576
David Anson <david@dlaa.me>
2023-02-22 22:55:56 -0800

    Add micromark-extension-gfm-table to markdownlint-micromark package exports.

--- micromark/exports.mjs
+++ micromark/exports.mjs
@@ -4,6 +4,7 @@
 
 export { gfmAutolinkLiteral } from "micromark-extension-gfm-autolink-literal";
 export { gfmFootnote } from "micromark-extension-gfm-footnote";
+export { gfmTable } from "micromark-extension-gfm-table";
 export { parse } from "micromark/lib/parse";
 export { postprocess } from "micromark/lib/postprocess";
 export { preprocess } from "micromark/lib/preprocess";
