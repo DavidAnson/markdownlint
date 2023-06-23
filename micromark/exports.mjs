8fb6287300ed45cd848a1b793de22c5a9698a795
David Anson <david@dlaa.me>
2023-06-23 02:36:23 +0000

    Update micromark import paths for changes in version 4, output any errors during webpack build.

--- micromark/exports.mjs
+++ micromark/exports.mjs
@@ -5,6 +5,6 @@
 export { gfmAutolinkLiteral } from "micromark-extension-gfm-autolink-literal";
 export { gfmFootnote } from "micromark-extension-gfm-footnote";
 export { gfmTable } from "micromark-extension-gfm-table";
-export { parse } from "micromark/lib/parse";
-export { postprocess } from "micromark/lib/postprocess";
-export { preprocess } from "micromark/lib/preprocess";
+export { parse } from "micromark";
+export { postprocess } from "micromark";
+export { preprocess } from "micromark";
