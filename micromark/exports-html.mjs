5f3d73a3b1110027dd13306379a759d95d52bbda
David Anson <david@dlaa.me>
2024-01-01 21:39:06 -0800

    Add micromark-extension-directive to markdownlint-micromark package exports.

--- micromark/exports-html.mjs
+++ micromark/exports-html.mjs
@@ -3,6 +3,7 @@
 /* eslint-disable n/no-missing-import */
 
 export { compile } from "micromark";
+export { directiveHtml } from "micromark-extension-directive";
 export { gfmAutolinkLiteralHtml } from "micromark-extension-gfm-autolink-literal";
 export { gfmFootnoteHtml } from "micromark-extension-gfm-footnote";
 export { gfmTableHtml } from "micromark-extension-gfm-table";
--- micromark/exports.mjs
+++ micromark/exports.mjs
@@ -2,6 +2,7 @@
 
 /* eslint-disable n/no-missing-import */
 
+export { directive } from "micromark-extension-directive";
 export { gfmAutolinkLiteral } from "micromark-extension-gfm-autolink-literal";
 export { gfmFootnote } from "micromark-extension-gfm-footnote";
 export { gfmTable } from "micromark-extension-gfm-table";
